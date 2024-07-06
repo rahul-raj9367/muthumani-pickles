b09d24504bbfc922a239f90915ac9133e5e46a64
Rahul Raj <rahulselvan0810.com>
2024-07-06 14:07:49 +0530

    text changes in about sections

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,69 +23,72 @@ export default function About() {
         <Box mt={["50px"]} >
             <Center><Heading as='h2' size={['xl','2xl','2xl','2xl']}>About Us</Heading></Center>
 
-            <Grid templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)']} gap={2} overflow="hidden" mt={["60px"]}>
+            <Grid display={["none","none","none","flex"]} templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)']} gap={2} overflow="hidden" mt={["60px"]}>
               <GridItem  w='100%' h='100%' data-aos="fade-right"
      data-aos-offset="300"
      data-aos-easing="ease-in-sine" data-aos-delay="500">
               <Center>
-                <Image src="images/PackiaSelvan.jpg"  loading="lazy" mt={["","","","0px",""]}   alt="Traditional Pickles" borderRadius="20px" width={["90%","90%","50%","70%","60%","50%"]} height={["100%","100%","100%","100%"]} ml={["0px","0px","0px"]}/>
+                <Image src="images/PackiaSelvan.jpg"  loading="lazy" mt={["","","","0px",""]}   alt="Traditional Pickles" borderRadius="20px" width={["90%","90%","50%","60%","60%","50%"]} height={["100%","100%","100%","100%"]} ml={["0px","0px","0px"]}/>
                 </Center>
               </GridItem>
               <GridItem w='100%' h='100%' data-aos="fade-left"
      data-aos-offset="300"
      data-aos-easing="ease-in-sine" data-aos-delay="500">
-              <Box w={['100%']} pl={["20px","20px","40px","20px"]} pr={["20px","20px","40px","20px"]} >
-                <Center>
-                <Text mt={["10%"]}  fontFamily="Ubuntu"
+                <Box  w={['100%','100%']} pl={["20px","20px","40px","20px"]} pr={["20px","20px","40px","20px"]} >
+                <>
+                <Text mt={["0%","0%","0%","0%","0%","5%"]}  fontFamily="Ubuntu"
                     fontWeight="400"
                     fontSize={["17px","17px","18px","18px","22px"]}
-                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    lineHeight={["40x","40x","38px","30px","40px"]}
                     textAlign="justify"
-                    marginBottom="20px" width={["100%","100%","90%"]}>
+                    marginBottom="20px" width={["100%","100%","90%","80%"]}>
                       At Muthumani Pickles, we bring you the rich and authentic flavors of traditional Indian pickles, crafted with care and passion. Founded by <span id="packi">Packia Selvan</span> and <span id="packi">Muthumani</span> on May 28, 2019, our company is dedicated to preserving the age-old art of pickle making, using only the finest natural ingredients and time-honored techniques.
                   </Text>
-                </Center>
-                <Center>
+                </>
+                <>
                 <Text   fontFamily="Ubuntu"
                     fontWeight="400"
                     fontSize={["17px","17px","18px","18px","22px"]}
-                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    lineHeight={["40x","40x","38px","30px","40px"]}
                     textAlign="justify"
-                    marginBottom="20px" width={["100%","100%","90%"]}>
+                    marginBottom="20px" width={["100%","100%","90%","80%"]}>
                       Join us in savoring the delightful and diverse flavors of Muthumani Pickles. Whether you're looking to add a spicy kick to your meals or a tangy twist to your snacks, our pickles are the perfect choice to elevate your dining experience. Welcome to a world of tradition and taste with Muthumani Pickles!
                   </Text>
-                </Center>
+                </>
                   
 
                 </Box>
+
               </GridItem>
 
 
             </Grid>
 
 
+            
+
             {/* Desktop */}
             <Grid display={["none","none","none","flex"]} templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(2, 1fr)']} gap={2} overflow="hidden" mt={["50px"]}>
-              <GridItem w='100%' h='100%' data-aos="fade-right"
+              <GridItem  w='100%' h='100%' data-aos="fade-right"
      data-aos-offset="300"
-     data-aos-easing="ease-in-sine" data-aos-delay="500">
-              <Box w={['100%']} pl={["20px","20px","40px","20px"]} pr={["20px","20px","40px","20px"]} >
-                <Center>
-                <Text mt={["10%"]}  fontFamily="Ubuntu"
+     data-aos-easing="ease-in-sine" data-aos-delay="500" textAlign="end" >
+              <Box  w={['100%']} pl={["20px","20px","40px","20px"]}  pr={["20px","20px","40px","20px"]} >
+                <Center ml="15%">
+                <Text mt={["0%","0%","0%","0%","0%","5%"]}  fontFamily="Ubuntu"
                     fontWeight="400"
                     fontSize={["17px","17px","18px","18px","22px"]}
-                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    lineHeight={["40x","40x","38px","30px","40px"]}
                     textAlign="justify"
-                    marginBottom="20px" width={["100%","100%","90%"]}>
+                    marginBottom="20px" width={["100%","100%","90%","90%"]}>
                   Our main focus is customer satisfaction, and we always prioritize feedback to improve the taste and quality of our products. This commitment to quality has earned us a reputation for excellence. As a Micro enterprise, we ensure that every product we offer meets your expectations through the highest standards in our manufacturing processes.</Text>
                 </Center>
-                <Center>
+                <Center ml="15%">
                 <Text   fontFamily="Ubuntu"
                     fontWeight="400"
                     fontSize={["17px","17px","18px","18px","22px"]}
-                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    lineHeight={["40x","40x","38px","30px","40px"]}
                     textAlign="justify"
-                    marginBottom="20px" width={["100%","100%","90%"]}>
+                    marginBottom="20px" width={["100%","100%","90%","90%"]}>
                       Located in Mukkudal, Tamil Nadu, Muthumani Pickles is a family-run business that prides itself on delivering quality and taste in every jar. Our pickles are more than just condiments; they are a celebration of India's culinary heritage. Each pickle is a blend of seasonal vegetables, carefully selected and preserved to enhance their natural flavors.
                   </Text>
                 </Center>
@@ -99,7 +102,7 @@ export default function About() {
      data-aos-offset="300"
      data-aos-easing="ease-in-sine" data-aos-delay="500">
               <Center>
-                <Image src="images/muthumani.jpg"  loading="lazy" mt={["","","","0px",""]}   alt="Traditional Pickles" borderRadius="20px" width={["90%","90%","50%","70%","60%","50%"]} height={["100%","100%","100%","50%"]} ml={["0px","0px","0px"]}/>
+                <Image src="images/muthumani.jpg"  loading="lazy" mt={["","","","0px",""]}   alt="Traditional Pickles" borderRadius="20px" width={["90%","90%","50%","60%","60%","50%"]} height={["100%","100%","100%","50%"]} ml={["0px","0px","0px"]}/>
                 </Center>
               </GridItem>
 
@@ -108,6 +111,50 @@ export default function About() {
 
 
             {/* Mobile */}
+            <Grid display={["flex","flex","flex","none"]} flexDir={["column"]} templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(2, 1fr)']} gap={2} overflow="hidden" mt={["50px"]}>
+              
+            <GridItem  w='100%' h='100%' data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" data-aos-delay="500">
+              <Center>
+                <Image src="images/PackiaSelvan.jpg"  loading="lazy" mt={["","","","0px",""]}   alt="Traditional Pickles" borderRadius="20px" width={["90%","90%","50%","70%","60%","50%"]} height={["100%","100%","100%","100%"]} ml={["0px","0px","0px"]}/>
+                </Center>
+              </GridItem>
+              
+              <GridItem w='100%' h='100%' data-aos="fade-left"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" data-aos-delay="500">
+              <Box w={['100%']} pl={["20px","20px","40px","20px"]} pr={["20px","20px","40px","20px"]} >
+                <Center>
+                <Text mt={["10%","10%","5%"]}    fontFamily="Ubuntu"
+                    fontWeight="400"
+                    fontSize={["17px","17px","18px","18px","22px"]}
+                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    textAlign="justify"
+                    marginBottom="20px" width={["100%","100%","90%"]}>
+                                       At Muthumani Pickles, we bring you the rich and authentic flavors of traditional Indian pickles, crafted with care and passion. Founded by <span id="packi">Packia Selvan</span> and <span id="packi">Muthumani</span> on May 28, 2019, our company is dedicated to preserving the age-old art of pickle making, using only the finest natural ingredients and time-honored techniques.
+                                       </Text>
+                </Center>
+                <Center>
+                <Text   fontFamily="Ubuntu"
+                    fontWeight="400"
+                    fontSize={["17px","17px","18px","18px","22px"]}
+                    lineHeight={["40x","40x","38px","30px","43px"]}
+                    textAlign="justify"
+                    marginBottom="20px" width={["100%","100%","90%"]}>
+                                           Join us in savoring the delightful and diverse flavors of Muthumani Pickles. Whether you're looking to add a spicy kick to your meals or a tangy twist to your snacks, our pickles are the perfect choice to elevate your dining experience. Welcome to a world of tradition and taste with Muthumani Pickles!
+                  </Text>
+                </Center>
+                  
+
+                </Box>
+              </GridItem>
+
+
+             
+
+
+            </Grid>
             <Grid display={["flex","flex","flex","none"]} flexDir={["column"]} templateColumns={['repeat(1, 1fr)','repeat(1, 1fr)','repeat(2, 1fr)','repeat(2, 1fr)']} gap={2} overflow="hidden" mt={["50px"]}>
               
             <GridItem  w='100%' h='100%' data-aos="fade-right"
@@ -123,7 +170,7 @@ export default function About() {
      data-aos-easing="ease-in-sine" data-aos-delay="500">
               <Box w={['100%']} pl={["20px","20px","40px","20px"]} pr={["20px","20px","40px","20px"]} >
                 <Center>
-                <Text mt={["10%"]}    fontFamily="Ubuntu"
+                <Text mt={["10%","10%","5%"]}    fontFamily="Ubuntu"
                     fontWeight="400"
                     fontSize={["17px","17px","18px","18px","22px"]}
                     lineHeight={["40x","40x","38px","30px","43px"]}

1b1b91ad45f44901a0da97b5a4507b580990562d
Rahul Raj <rahulselvan0810.com>
2024-07-08 15:42:49 +0530

    add 404 page

--- src/pages/PageError.jsx
+++ src/pages/PageError.jsx
@@ -2,6 +2,7 @@ import { Box,Center,Image,Heading ,Text,Tooltip,Button} from '@chakra-ui/react'
 import React, { useEffect } from 'react';
 import { NavLink,Link } from "react-router-dom";
 import { GoRead } from "react-icons/go";
+import { FaHome } from "react-icons/fa";
 export default function PageError({setActiveButton}) {
     useEffect(() => {
         document.title = '404 Page Not Found - MuthuMani Home Made Pickles and Turmeric Powder';
@@ -15,7 +16,7 @@ export default function PageError({setActiveButton}) {
       <Center> <Image  src='images/oops.jpg' alt='404 Page Error' height={["400","500"]} width={["500"]} /></Center>
       <Center><Heading color="black" as='h2' size={['lg','2xl','2xl','2xl']} mt="15px">This page doesn’t exist</Heading></Center>
       <Center><Text mt="15px"  color="#B31312" as="b">Please check your URL or return to home.</Text></Center>
-      <Center><Link to="/"><Button bg='#B31312' mt="15%" _hover={{ bg: 'black', color:'#ffff' }} ml={["","","5%","5%"]} color='#ffffff' leftIcon={<GoRead size={20} />}  onClick={() => setActiveButton(1)}>Back to Home</Button></Link></Center>
+      <Center><Link to="/"><Button bg='#B31312' mt="15%" _hover={{ bg: 'black', color:'#ffff' }} ml={["","","5%","5%"]} color='#ffffff' leftIcon={<FaHome size={20} />}  onClick={() => setActiveButton(1)}>Back to Home</Button></Link></Center>
     </Box>
       
     </Box>
